934e13d558a3f2347009cf461a844ca9b3bfdaa4
nishizawa-kouhei-28 <94812190+nishizawa-kouhei-28@users.noreply.github.com>
2021-12-02 19:03:57 +0900

    Initial Commit

---
+++ mattibou/mattibouApp.swift
@@ -0,0 +1,17 @@
+//
+//  mattibouApp.swift
+//  mattibou
+//
+//  Created by 西澤 耕平 on 2021/12/02.
+//
+
+import SwiftUI
+
+@main
+struct mattibouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
